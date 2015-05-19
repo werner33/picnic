fa1bde56117de837143b0fe97e720df1f3dc6b8f
Jordan <jordanmmanley@gmail.com>
2015-05-19 19:35:34 +0000

    log in on navbar

---
+++ app/controllers/invite_controller.rb
@@ -0,0 +1,48 @@
+class InvitesController < ApplicationController
+  before_action :set_invite, only: [:show, :edit, :update, :destroy]
+
+  respond_to :html
+
+  def index
+    @invites = Invite.all
+    respond_with(@invites)
+  end
+
+  def show
+  @invite = Invite.find(params[:id])
+    respond_with(@invite)
+  end
+
+  def new
+    @invite = Invite.new
+    respond_with(@invite)
+  end
+
+  def edit
+  end
+
+  def create
+    @invite = Invite.new(invite_params)
+    @invite.save
+    respond_with(@invite)
+  end
+
+  def update
+    @invite.update(invite_params)
+    respond_with(@invite)
+  end
+
+  def destroy
+    @invite.destroy
+    respond_with(@invite)
+  end
+
+  private
+    def set_invite
+      @invite = Invite.find(params[:id])
+    end
+
+    def invite_params
+      params.require(:invite).permit(:user_id, :picnic_id)
+    end
+end
