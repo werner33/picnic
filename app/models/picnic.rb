b05391ddbb75bf6b45249b8a00c735724420b68e
Jordan <jordanmmanley@gmail.com>
2015-05-20 04:18:17 +0000

    picnics are created by users as hosts

--- app/models/picnic.rb
+++ app/models/picnic.rb
@@ -1,5 +1,7 @@
 class Picnic < ActiveRecord::Base
-
+  
+  
+  belongs_to :host, :class_name => "User"
   has_many :invites
   has_many :users, through: :invites
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150514163658) do
+ActiveRecord::Schema.define(version: 20150519201826) do
 
   create_table "invites", force: true do |t|
     t.integer  "user_id"
@@ -31,6 +31,7 @@ ActiveRecord::Schema.define(version: 20150514163658) do
     t.string   "location"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "host_id"
   end
 
   create_table "users", force: true do |t|
