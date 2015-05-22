f2ca9dd1e988baf36d8234e525417292447de7bd
Jordan <jordanmmanley@gmail.com>
2015-05-22 14:50:52 +0000

    add names to users

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   resources :picnics
   resources :invites
-  devise_for :users
+  devise_for :users,  :controllers => { registrations: 'registrations' }
   root 'picnics#index'
   
   # The priority is based upon order of creation: first created -> highest priority.
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150519201826) do
+ActiveRecord::Schema.define(version: 20150522142246) do
 
   create_table "invites", force: true do |t|
     t.integer  "user_id"
@@ -47,6 +47,8 @@ ActiveRecord::Schema.define(version: 20150519201826) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
