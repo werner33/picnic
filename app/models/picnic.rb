1f79ab7955c53beae99f4caba85a41b4f5bdfc13
Jordan <jordanmmanley@gmail.com>
2015-05-22 17:49:20 +0000

    make invite dependent on picnic

--- app/models/picnic.rb
+++ app/models/picnic.rb
@@ -3,8 +3,10 @@ class Picnic < ActiveRecord::Base
   
     
   belongs_to :host, :class_name => "User"
-  has_many :invites
+  has_many :invites, dependent: :destroy
   has_many :users, through: :invites
+  
+  
   
   accepts_nested_attributes_for :invites
   
