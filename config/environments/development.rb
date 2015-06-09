adf50f9eb9b4714a107ef2a4f9553dedf5e252f1
Jordan <jordanmmanley@gmail.com>
2015-06-09 12:58:32 +0000

    devise mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,4 +34,21 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+  
+  #devise
+  config.action_mailer.default_url_options = { :host => 'picnic-werner33.c9.io' }
+  # Rails.application.routes.default_url_options[:host] = 'picnic-werner33.c9.io'
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+  address: "smtp.gmail.com",
+  port: 587,
+  domain: ENV["GMAIL_DOMAIN"],
+  authentication: "plain",
+  enable_starttls_auto: true,
+  user_name: ENV["GMAIL_USERNAME"],
+  password: ENV["GMAIL_PASSWORD"]
+  }
+
 end
