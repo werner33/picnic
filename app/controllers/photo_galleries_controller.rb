a09f541acc84a03f86cd32fda54dfb23171e00a1
Jordan <jordanmmanley@gmail.com>
2015-06-25 14:01:28 +0000

    change gallery controller

--- app/controllers/photo_galleries_controller.rb
+++ app/controllers/photo_galleries_controller.rb
@@ -9,38 +9,37 @@ class PhotoGalleriesController < ApplicationController
   end 
 
     
-    def edit 
-      @photo_gallery = PhotoGallery.find(params[:id])
-      @photo = @photo_gallery.photos.build
-    end 
+  def edit 
+    @photo_gallery = PhotoGallery.find(params[:id])
+    @photo = @photo_gallery.photos.build
+  end 
 
 
     
-    def update
-      @photo_gallery = PhotoGallery.find(params[:id])
-    #   @photos = @photo_gallery.photos.build(photo_gallery_params)
-      
-      if @photo_gallery && params[:photos]
-          params[:photos]['image'].each do |a|
-              @photo = @photo_gallery.photos.create!(:image => a, 
-              :photo_gallery_id => @photo_gallery.id )
-          end 
-         respond_to do |format|
-            format.html { redirect_to @photo_gallery, notice: 'Photo upload successful.' }
-         end 
-      else 
-          respond_to do |format|
-            format.html { render action: 'show', alert: 'No photos were uploaded.'}
-          end 
-      end 
+  def update
+    @photo_gallery = PhotoGallery.find(params[:id])
+
+    if @photo_gallery && params[:photos]
+        params[:photos]['image'].each do |a|
+            @photo = @photo_gallery.photos.create!(:image => a, 
+            :photo_gallery_id => @photo_gallery.id )
+        end 
+       respond_to do |format|
+          format.html { redirect_to @photo_gallery, notice: 'Photo upload successful.' }
+       end 
+    else 
+        respond_to do |format|
+          format.html { render action: 'show', alert: 'No photos were uploaded.'}
+        end 
     end 
+  end 
 
 
 
  private
-   def post_params
-      params.require(:post).permit(:title, photos_attributes: [:id, :photo_gallery_id, :image])
-   end
+    def photo_gallery_params
+      params.require(:photo).permit(:title, photos_attributes: [:id, :photo_gallery_id, :image])
+    end
 
 
 end 
